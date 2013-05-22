f5f8b7d9dd1a0626775745b7e88943d975c2f95f
Gareth Senior <gareth53@gmail.com>
2013-05-22 18:11:06 +0100

    adding basic http authentication for feed retrieval

--- gareth53_v2/apps/lifestream/management/commands/import_feed.py
+++ gareth53_v2/apps/lifestream/management/commands/import_feed.py
@@ -1,6 +1,6 @@
-import feedparser
 import datetime
 from optparse import make_option
+import urllib2, feedparser
 
 from django.core.management.base import NoArgsCommand
 
@@ -18,12 +18,25 @@ class Command(NoArgsCommand):
    
     def handle_noargs(self, **options):
         slug = options.get('slug', '')
+        verbose = options.get('verbose', '')
+        auth = None
         if slug:
             source = Source.objects.get(slug=slug)
         else:
             # gets the most stale feed
             source = Source.objects.filter(url__isnull=False).order_by('last_check')[0]
-        # TODO - handle RSS feeds that require authentication
-        contents = feedparser.parse(source.url)
+        if source.username and source.password:
+            auth = urllib2.HTTPBasicAuthHandler()
+            # Add password information: realm, host, user, password.
+            auth.add_password(source.name, get_domain(source.url), source.username, source.password)
+            contents = feedparser.parse(source.url, handlers=[auth])            
+        else:
+            contents = feedparser.parse(source.url)
+        if verbose:
+            print "Processing feed for %s" % source.name
         create_items(source, contents)
 
+def get_domain(full_url):
+    url = full_url.replace('http://', '')
+    return url[0:url.find('/')]
+    
