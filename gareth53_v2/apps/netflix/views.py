a2085d7fd6e3ea03bbe9ab1d31778705803bf521
Gareth Senior <gareth.senior@thisisglobal.com>
2016-01-05 14:38:10 +0000

    data is pretty flat, so have simplified the data structure from JSON to individula vars linked by a numerical suffix. Lovely...

--- gareth53_v2/apps/netflix/views.py
+++ gareth53_v2/apps/netflix/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 from gareth53_v2.apps.lifestream.models import Source, Item
 from .forms import NetflixForm
 
-
 def item_form(request):
     ctxt = {
         'form': NetflixForm()
@@ -19,6 +18,7 @@ def item_form(request):
         item_count = int(data.get('items', '1'))
         for x in range(1, item_count+1):
             pub_date = datetime.strptime(data['pub_date%d' % x], '%Y-%m-%dT%H:%M')
+            # TODO: add domain name if URL is just a path
             item, created = Item.objects.get_or_create(feed=source,
                                                        title=data['title%d' % x],
                                                        url=data['url%d' % x],
@@ -31,9 +31,18 @@ def item_form(request):
             'createds': createds,
             'duplicates': duplicates
             })
-    elif request.GET.get('data', ''):
-        items = json.loads(request.GET.get('data', '')).get('items', [])
-        ctxt.update({
-            'form': NetflixForm(extra=items)
-        })
-    return render(request, 'netflix/items_create.html', ctxt)    
+    elif request.GET:
+        data = request.GET
+        item_count = int(data.get('items', 0))
+        if item_count:
+            items = []
+            for x in xrange(1, item_count+1):
+                items.append({
+                    'title': data.get('title%s' % x, None),
+                    'pub_date': data.get('pub_date%s' % x, None),
+                    'url': data.get('url%s' % x, None)
+                })
+            ctxt.update({
+                'form': NetflixForm(extra=items)
+            })
+    return render(request, 'netflix/items_create.html', ctxt)
